00926054ace43a670ed156ba75cfbe81c67f1f46
RandyLDP <80972485+RandyLDP@users.noreply.github.com>
2021-05-11 14:54:32 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+
+console.log('Hello Winc Academy');
+ let name = 'Randy';
+ let age = 27;
+ console.log (name);
+ console.log  (age);
+
+
+
+        function addition(number){
+           return number + number;
+        }
+        console.log(addition(2));
